cd2c0b4eaa7e6d4f437b2a38a9bd844f1a4df3b6
sins621 <bradlycarpenterza@gmail.com>
2025-02-16 20:47:07 +0200

    update/add-search-and-add-to-que

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
@@ -23,15 +24,34 @@ class SpotifyActions:
 
     def now_playing(self):
         current_playback = self.sp.current_playback()
+        print(json.dumps(current_playback, indent=2))
         if current_playback:
+
             return {
-                "song": current_playback["item"]["external_urls"]["spotify"],
-                "playlist": current_playback["context"]["external_urls"]["spotify"],
+                "song_link": current_playback["item"]["external_urls"]["spotify"],
+                "playlist_link": current_playback["context"]["external_urls"][
+                    "spotify"
+                ],
+                "artists": [
+                    artist_data["name"]
+                    for artist_data in current_playback["item"]["artists"]
+                ],
+                "song_name": current_playback["item"]["name"],
             }
 
     def skip_song(self):
         self.sp.next_track()
+        return {"message": "Skipped Song"}
 
-    def add_to_queue(self, uri):
-        if uri:
-            self.sp.add_to_queue(uri)
+    def search(self, args):
+        request = self.sp.search(q=args["q"], limit=1)
+        song_data = request["tracks"]["items"][0]  # pyright:ignore
+        self.add_to_queue(song_data["uri"])
+        return {
+            "song_name": song_data["name"],
+            "artists": [artist_data["name"] for artist_data in song_data["artists"]],
+        }
+
+    def add_to_queue(self, url):
+        if url:
+            self.sp.add_to_queue(url)
